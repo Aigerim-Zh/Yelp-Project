2d719b71d9fc4f4dc8b3b4e67cba8b6e32f14b39
Hristo Bustamante <hristobustamante@Hristos-MBP.fios-router.home>
2022-05-14 21:07:39 -0400

    Adding the sql file that will be used for creating database and tables

---
+++ yelp_database.sql
@@ -0,0 +1,18 @@
+-- code for setting up database.
+
+-- Create Business Table
+-- primary key Business_ID
+
+
+-- Create Reviews Table
+-- primary key Reviews_ID
+-- FOREIGN KEY (Business_ID) REFERENCES business (Business_ID)--
+
+
+-- Create User Table
+-- primary key User_ID
+ -- FOREIGN KEY (User_ID) REFERENCES reviews (User_ID)--
+
+
+
+
