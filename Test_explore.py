522b342ed6a4d71e662e2d57c648a81d840e228e
Hristo Bustamante <hristobustamante@Hristos-MBP.fios-router.home>
2022-05-15 18:00:49 -0400

    Adding the sql code for setting up the database and screenshot of reviews table

--- Test_explore.py
+++ Test_explore.py
@@ -1,1 +1,11 @@
-#This is to test any ideas or attributes for the project.
+#This is to test any ideas or attributes for the project with datascience libraries .
+import numpy as np
+import pandas as pd
+from pathlib import Path
+from collections import Counter
+from sklearn.metrics import balanced_accuracy_score
+from sklearn.metrics import confusion_matrix
+
+
+
+
